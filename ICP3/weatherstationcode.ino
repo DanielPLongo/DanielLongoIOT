d7fa52d503723e025523bc14f0c47516da97e9b5
Daniel Longo <daniel.longo@garmin.com>
2019-02-08 23:24:09 -0600

    deleted old code

--- ICP3/weatherstationcode.ino
+++ ICP3/weatherstationcode.ino
@@ -17,10 +17,26 @@ DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 #include <Adafruit_BMP280.h>
 Adafruit_BMP280 bmp; // I2C
 
+//********Light***********
+int light;
+
+//********Dust***********
+int pin = 8;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 2000; 
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+
+//********UV***********
+float Vsig;
+
+
 #define SSID "490RoProg1"     // "SSID-WiFiname" 
 #define PASS "490RoPro"       // "password"
 #define IP "184.106.153.149"// thingspeak.com ip
-String msg = "GET /update?key=OTLPPT7RD92WH3GE"; //change it with your api key like "GET /update?key=Your Api Key"
+String msg = "GET /update?key=CQ5PC2S1M5ZC34VB"; //change it with your api key like "GET /update?key=Your Api Key"
 
 
 //Variables
@@ -32,8 +48,6 @@ int pulsePin = 0;                 // Pulse Sensor purple wire connected to analo
 int blinkPin = 13;                // pin to blink led at each beat
 int fadePin = 5;
 int fadeRate = 0;
-int BPM = 0;
-int Signal = 0;
 
 // Regards Serial OutPut  -- Set This Up to your needs
 static boolean serialVisual = true;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse 
@@ -49,28 +63,24 @@ void setup()
   lcd.print("Connecting...");
   Serial.begin(115200); //or use default 115200.
   esp8266.begin(115200);
-  lcd.println("AT");
+  Serial.println("AT");
   esp8266.println("AT");
   delay(5000);
   if(esp8266.find("OK")){
     connectWiFi();
   }
-  interruptSetup(); 
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(2, INPUT);
-  pinMode(6, INPUT);
-  pinMode(7, INPUT);
-  digitalWrite(3, LOW); //Set lights to off initially
-  digitalWrite(4,LOW);
-  digitalWrite(5,LOW);
-  //lcd.noDisplay();
+ 
+  
+  pinMode(A0,INPUT); //pin for Light sensor
+
+  pinMode(8,INPUT); //pin for Dust sensor
+
+
 }
 
 void loop(){
   lcd.clear();
-  
+  lcd.setCursor(0,0);
  //Temp Sensor
     //Read data and store it to variables hum and temp
     hum = dht.readHumidity();
@@ -79,31 +89,130 @@ void loop(){
     lcd.print("Humidity: ");
     lcd.print(hum);
     lcd.print("%");
-    lcd.setCursor(0, 2); // set the cursor to column 0, line 2
+    lcd.setCursor(0, 1); // set the cursor to column 0, line 2
     lcd.print("Temp: ");
     lcd.print(temp);
     lcd.println(" *C");
     delay(2000); //Delay 2 sec.
+    updatebeat('1', hum);
+    updatebeat('2', temp);
+    
 
 //Barometer
-    lcd.print(F("Temperature = "));
-    lcd.print(bmp.readTemperature());
-    lcd.println(" *C");
+    lcd.clear();
+    lcd.setCursor(0,0);
+//    lcd.print(F("Temperature = "));
+//    lcd.print(bmp.readTemperature());
+//    lcd.println(" *C");
     
     lcd.print(F("Pressure = "));
     lcd.print(bmp.readPressure());
     lcd.println(" Pa");
-
-    lcd.print(F("Approx altitude = "));
-    lcd.print(bmp.readAltitude(1013.25)); // this should be adjusted to your local forcase
-    lcd.println(" m");
-    
+    lcd.setCursor(0,1);
+    lcd.print(F("Altitude = "));
+    lcd.print(bmp.readAltitude(29.92)); // this should be adjusted to your local forcase
+    lcd.println(" m");    
     lcd.println();
+    updatebeat('3', bmp.readPressure());
+    updatebeat('4', bmp.readAltitude(29.92));    
     delay(2000);
+
+//Light Sensor
+  lcd.clear();
+  lcd.setCursor(0,0);
+  light=analogRead(A0);
+  lcd.println("Light = " + String(light));
+  updatebeat('5', light);
+  delay(2000);    
+
+
+//Dust Sensor
+  lcd.clear();
+  lcd.setCursor(0,0);
+  starttime = millis();
+  delay(2000);
+  duration = pulseIn(pin, LOW);
+  lowpulseoccupancy = lowpulseoccupancy+duration;
+  if ((millis()-starttime) >= sampletime_ms) //if the sampel time = = 30s
+  {
+    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; 
+    lcd.print("CX = ");
+    lcd.print(concentration);
+    lcd.println(" pcs/0.01cf");
+    lcd.println("\n");
+    lowpulseoccupancy = 0;
+    updatebeat('6', concentration);
+    delay(2000); 
+  }
+
+
+//UV Sensor
+  int sensorValue;
+  long  sum=0;
+  for(int i=0;i<1024;i++)
+   {  
+      sensorValue=analogRead(A1);
+      sum=sensorValue+sum;
+      delay(2);
+   }   
+ sum = sum >> 10;
+ Vsig = sum*4980.0/1023.0; // Vsig is the value of voltage measured from the SIG pin of the Grove interface
+ lcd.clear();
+ lcd.setCursor(0,0);
+ lcd.print("UV Level: ");
+ lcd.print(Vsig);
+ lcd.print(" mV    --   ");
+ 
+//if (Vsig < 50) {
+//    Serial.print("UV Index: 0 "); Serial.println("   Exposure level - NONE (You're probably at home!) ");
+// }
+//if (Vsig > 50 && Vsig < 227) {
+//    Serial.print("UV Index: 1 "); Serial.println("   Exposure level - LOW (You're probably at home!) ");
+// }
+//if (Vsig > 227 && Vsig < 318) {
+//    Serial.print("UV Index: 2 "); Serial.println("   Exposure level - LOW (You can go outside and have fun!) ");
+// }
+//if (Vsig > 318 && Vsig < 408) {
+//    Serial.print("UV Index: 3 "); Serial.println("   Exposure level - MODERATE (Sun starts to annoy you) ");
+// }
+//if (Vsig > 408 && Vsig < 503) {
+//    Serial.print("UV Index: 4 "); Serial.println("   Exposure level - MODERATE (Sun starts to annoy you) ");
+// }
+//if (Vsig > 503 && Vsig < 606) {
+//    Serial.print("UV Index: 5 "); Serial.println("   Exposure level - MODERATE (Sun starts to annoy you) ");
+// }
+//if (Vsig > 606 && Vsig < 696) {
+//    Serial.print("UV Index: 6 "); Serial.println("   Exposure level - HIGH (Get out from the sunlight! get out now!) ");
+// }
+//if (Vsig > 696 && Vsig < 795) {
+//    Serial.print("UV Index: 7 "); Serial.println("   Exposure level - HIGH (Get out from the sunlight! get out now!) ");
+// }
+//if (Vsig > 795 && Vsig < 881) {
+//    Serial.print("UV Index: 8 "); Serial.println("   Exposure level - VERY HIGH (Get out from the sunlight! get out now!) ");
+// }
+//if (Vsig > 881 && Vsig < 976) {
+//    Serial.print("UV Index: 9 "); Serial.println("   Exposure level - VERY HIGH (If you value your health, don't go outside, just stay at home!) ");
+// }
+//if (Vsig > 976 && Vsig < 1079) {
+//    Serial.print("UV Index: 10 "); Serial.println("   Exposure level - VERY HIGH (If you value your health, don't go outside, just stay at home!) ");
+// }
+//if (Vsig > 1079 && Vsig < 1170) {
+//    Serial.print("UV Index: 11 "); Serial.println("   Exposure level - EXTREME (If you value your health, don't go outside, just stay at home!) ");
+// }
+//if (Vsig > 1170) {
+//    Serial.print("UV Index: 11+ "); Serial.println("   Exposure level - EXTREME (You will probably die in 3, 2, 1... Just JOKING, don't be scared...) - intensity of sunlight is really at maximum ");
+// }
+    updatebeat('7',Vsig);
+    delay(2000);
+
+
+
+
   start: //label 
   error=0;
   delay(100);
-  updatebeat();
+  //updatebeat();
   //Resend if transmission is not completed 
   if (error==1){
     goto start; //go to label "start"
@@ -111,26 +220,28 @@ void loop(){
   delay(100); 
 }
 
-void updatebeat(){
+void updatebeat(char field, float value){
   String cmd = "AT+CIPSTART=\"TCP\",\"";
   cmd += IP;
   cmd += "\",80";
-  lcd.println(cmd);
+  Serial.println(cmd);
   esp8266.println(cmd);
-  delay(2000);
+  delay(200);
   if(esp8266.find("Error")){
     return;
   }
   cmd = msg ;
-  cmd += "&field1=";   
-  cmd += BPM;
+  cmd += "&field";
+  cmd += field;
+  cmd += "=";
+  cmd += value;
   cmd += "\r\n";
-  lcd.print("AT+CIPSEND=");
+  Serial.print("AT+CIPSEND=");
   esp8266.print("AT+CIPSEND=");
-  lcd.println(cmd.length());
+  Serial.println(cmd.length());
   esp8266.println(cmd.length());
  // if(esp8266.find(">")){
-    lcd.print(cmd);
+    Serial.print(cmd);
     esp8266.print(cmd);
  // }
   //else{
@@ -139,10 +250,11 @@ void updatebeat(){
    // //Resend...
    // error=1;
   //}
+  delay(500);
 }
 
 boolean connectWiFi(){
-  lcd.println("AT+CWMODE=1");
+  Serial.println("AT+CWMODE=1");
   esp8266.println("AT+CWMODE=1");
   delay(2000);
   String cmd="AT+CWJAP=\"";
@@ -150,11 +262,11 @@ boolean connectWiFi(){
   cmd+="\",\"";
   cmd+=PASS;
   cmd+="\"";
-  lcd.println(cmd);
+  Serial.println(cmd);
   esp8266.println(cmd);
-  delay(5000);
+  delay(3000);
   if(esp8266.find("OK")){
-    lcd.println("OK");
+    Serial.println("OK");
     return true;    
   }else{
     return false;
@@ -168,96 +280,3 @@ void interruptSetup(){
   TIMSK2 = 0x02;     // ENABLE INTERRUPT ON MATCH BETWEEN TIMER2 AND OCR2A
   sei();             // MAKE SURE GLOBAL INTERRUPTS ARE ENABLED      
 } 
-
-ISR(TIMER2_COMPA_vect){                       // triggered when Timer2 counts to 124
-  cli();                                      // disable interrupts while we do this
-  Signal = analogRead(pulsePin);              // read the Pulse Sensor 
-  sampleCounter += 2;                         // keep track of the time in mS
-  int N = sampleCounter - lastBeatTime;       // monitor the time since the last beat to avoid noise
-
-    //  find the peak and trough of the pulse wave
-  if(Signal < thresh && N > (IBI/5)*3){      // avoid dichrotic noise by waiting 3/5 of last IBI
-    if (Signal < T){                         // T is the trough
-      T = Signal;                            // keep track of lowest point in pulse wave 
-    }
-  }
-
-  if(Signal > thresh && Signal > P){        // thresh condition helps avoid noise
-    P = Signal;                             // P is the peak
-  }                                         // keep track of highest point in pulse wave
-
-  //  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
-  // signal surges up in value every time there is a pulse
-  if (N > 250){                                   // avoid high frequency noise
-    if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) ){        
-      Pulse = true;                               // set the Pulse flag when there is a pulse
-      digitalWrite(blinkPin,HIGH);                // turn on pin 13 LED
-      IBI = sampleCounter - lastBeatTime;         // time between beats in mS
-      lastBeatTime = sampleCounter;               // keep track of time for next pulse
-
-      if(secondBeat){                        // if this is the second beat
-        secondBeat = false;                  // clear secondBeat flag
-        for(int i=0; i<=9; i++){             // seed the running total to get a realistic BPM at startup
-          rate[i] = IBI;                      
-        }
-      }
-
-      if(firstBeat){                         // if it's the first time beat is found
-        firstBeat = false;                   // clear firstBeat flag
-        secondBeat = true;                   // set the second beat flag
-        sei();                               // enable interrupts again
-        return;                              // IBI value is unreliable so discard it
-      }   
-      word runningTotal = 0;                  // clear the runningTotal variable    
-
-      for(int i=0; i<=8; i++){                // shift data in the rate array
-        rate[i] = rate[i+1];                  // and drop the oldest IBI value 
-        runningTotal += rate[i];              // add up the 9 oldest IBI values
-      }
-
-      rate[9] = IBI;                          // add the latest IBI to the rate array
-      runningTotal += rate[9];                // add the latest IBI to runningTotal
-      runningTotal /= 10;                     // average the last 10 IBI values 
-
-      QS = true;                              // set Quantified Self flag 
-      // QS FLAG IS NOT CLEARED INSIDE THIS ISR
-      if(BPM<80){ //orange
-        digitalWrite(3, HIGH);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
-      }
-      else if(BPM>100){ //Red
-          digitalWrite(3, LOW);
-          digitalWrite(4, HIGH);
-          digitalWrite(5, LOW);
-      }
-      else{ //green
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, HIGH);
-        
-      }
-    }                       
-  }
-
-  if (Signal < thresh && Pulse == true){   // when the values are going down, the beat is over
-    digitalWrite(blinkPin,LOW);            // turn off pin 13 LED
-    Pulse = false;                         // reset the Pulse flag so we can do it again
-    amp = P - T;                           // get amplitude of the pulse wave
-    thresh = amp/2 + T;                    // set thresh at 50% of the amplitude
-    P = thresh;                            // reset these for next time
-    T = thresh;
-  }
-
-  if (N > 2500){                           // if 2.5 seconds go by without a beat
-    thresh = 512;                          // set thresh default
-    P = 512;                               // set P default
-    T = 512;                               // set T default
-    lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
-    firstBeat = true;                      // set these to avoid noise
-    secondBeat = false;                    // when we get the heartbeat back
-  }
-
-  sei();     
-  // enable interrupts when youre done!
-}// end
